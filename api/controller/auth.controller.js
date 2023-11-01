22a4dbc8bc1205021f9b62416de5583cc5c8751f
murshidameen <murshidmonzpv@gmail.com>
2023-11-01 16:11:45 +0530

    completed signup page UI

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,14 +1,17 @@
 import UserDb from '../models/userSchema.js'
+import bcryptjs from 'bcryptjs'
+
 export const signUp = async (req, res) => {
-    try {
-        const { name, email, password } = req.body;
-        const newUser = new UserDb({ name, email, password })
+    const { name, email, password } = req.body;
+    const hashedPassword = bcryptjs.hashSync(password,10)
+    try { 
+        const newUser = new UserDb({ name, email, password:hashedPassword })
         await newUser.save()
         res.status(200).json({"message":"User registered succesfully"})
 
     } catch (error) {
         console.log(error);
-        res.status(500).send("Internal")
+        next(error)
     }
     
 }
--- api/index.js
+++ api/index.js
@@ -12,8 +12,20 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/', userRouter)
-app.use('/api/auth',authRouter)
+app.use('/api/auth', authRouter)
 
+
+app.use ((err, req, res, next) => {
+    const statusCode = err.statusCode  || 500;
+    const message = err.message || "Internal Server Error!"
+    return res.status(statusCode).json(
+        {
+            success: false,
+            message,
+          statusCode
+        });
+}
+)
 
 
 app.listen(port, () => {
