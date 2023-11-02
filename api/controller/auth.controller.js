30869de7b715b02debf0d1545d10ccd14563bae9
murshidameen <murshidmonzpv@gmail.com>
2023-11-02 15:03:39 +0530

    created signin api route and jwr auth

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,5 +1,9 @@
-import UserDb from '../models/userSchema.js'
-import bcryptjs from 'bcryptjs'
+import userDb from '../models/userSchema.js';
+import UserDb from '../models/userSchema.js';
+import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config()
 
 export const signUp = async (req, res,next) => {
     const { name, email, password } = req.body;
@@ -14,4 +18,30 @@ export const signUp = async (req, res,next) => {
         next(error)
     }
     
+}
+
+export const signIn = async (req, res,next) => {
+    const { email, password, } = req.body;
+    try {
+        const validUser = await userDb.findOne({ email })
+        if (!validUser) {
+            return res.status(404).json({"message":"User not found!"})
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password)
+        if (!validPassword) {
+            return res.status(401).json({"message":"Invalid Credentials!"})
+        }
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+         // Exclude password when sending user data
+        const userToSend = validUser.toObject();
+        delete userToSend.password;
+        const expiryDate = new Date(Date.now()+3600000)
+        res.cookie('access_token', token, { httpOnly: true, expires:expiryDate})
+            .status(200)
+            .json(userToSend)
+        
+    } catch (err) {
+        console.log(err);
+        next(err)
+    }
 }
