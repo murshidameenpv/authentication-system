19a32c03be0ea51da42dd90f02e0be28cb2b39b6
murshidameen <murshidmonzpv@gmail.com>
2023-11-02 17:53:25 +0530

    added redux-persist to logged innuser even after the browser is closed

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,7 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import userReducer from './userSlice/userSlice.js'
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage';
+import persistStore from 'redux-persist/es/persistStore';
+import { FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+
+const rootReducer = combineReducers({ user: userReducer });
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage
+}
+
+const persistedReducer = persistReducer(persistConfig,rootReducer) 
 
 export const store = configureStore({
-    reducer:{user:userReducer},
-    
+    reducer:persistedReducer,
+    middleware: getDefaultMiddleware({
+        serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+    }),
 })
+
+export const persistor = persistStore(store)
