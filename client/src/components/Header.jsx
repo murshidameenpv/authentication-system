eb10e3075d64293b27320cc2e0b98ddcd7ea720e
murshidameen <murshidmonzpv@gmail.com>
2023-11-03 03:25:17 +0530

    added profile image upload functionality using uderRef

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
-import {useSelector} from 'react-redux'
+import { useSelector } from 'react-redux';
+
 export default function Header() {
-    const { currentUser } = useSelector((state)=>state.user)
+    const { currentUser } = useSelector((state) => state.user);
     return (
         <div className="bg-slate-300">
             <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -13,20 +14,18 @@ export default function Header() {
                     <Link to='/about'>
                         <li className="text-black hover:text-blue-500 transition duration-200">About</li>
                     </Link>
-                    <Link to='sign-in'>
-                        {currentUser ?
-                            (
-                                <Link to='/profile'>
-                                    <img src={currentUser.profileImage} alt="profile image" className="w-9 h-9 rounded-full object-cover    "/>
-                                </Link>
-                            ) :
-                            (
-                                <Link to='/sign-in'>
-                                    <li className="text-black hover:text-blue-500 transition duration-200">Sign In</li>
-                                </Link>
-                            )
-                        }
-                    </Link>
+                    {currentUser ?
+                        (
+                            <Link to='/profile'>
+                                <img src={currentUser.profileImage} alt="profile image" className="w-9 h-9 rounded-full object-cover"/>
+                            </Link>
+                        ) :
+                        (
+                            <Link to='/sign-in'>
+                                <li className="text-black hover:text-blue-500 transition duration-200">Sign In</li>
+                            </Link>
+                        )
+                    }
                 </ul>
             </div>
         </div>
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,13 +1,52 @@
-import { useSelector } from "react-redux"
+import { useSelector } from "react-redux";
+import { useRef, useState, useEffect } from "react";
+import { storage } from "../services/firebase";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 
 export default function Profile() {
-  const {currentUser} = useSelector((state)=>state.user)
+  const { currentUser } = useSelector((state) => state.user)
+  const fileRef = useRef(null);
+  const [image, setImage] = useState(undefined);
+  const [imagePercent, setImagePercent] = useState(0);
+  const [imageError, setImageError] = useState(false);
+  const [formData, setFormData] = useState({})
+  
+  useEffect(() => {
+    if (image) {
+      handleImageUpload(image)
+    }
+  }, [image]);
+
+  const handleImageUpload = async () => {
+    const fileName = new Date().getTime() + image.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, image);
+    uploadTask.on('state_changed', (snapshot) => {
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      setImagePercent(Math.floor(progress))
+    },
+    (error) => {
+      setImageError(true)
+    },
+    () => {
+     getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) => 
+        setFormData({...formData,profileImage:downloadUrl}))
+    });
+  }
+  
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-10">Profile</h1>
       <form className="flex flex-col gap-4">
-        <img src={currentUser.profileImage} alt="Profile Image" className="h-40 w-40 rounded-full self-center  cursor-pointer object-cover mt-2" />
-         <input defaultValue={currentUser.name} type="text" placeholder="Username" id="name" className="bg-slate-100 p-3 rounded-lg" />
+        <input type="file" ref={fileRef} hidden accept="image/*" onChange={(e)=>setImage(e.target.files[0])}/>
+        <img src={currentUser.profileImage} alt="Profile Image"
+          className="h-40 w-40 rounded-full self-center  cursor-pointer object-cover mt-2" onClick={()=>fileRef.current.click()}/>
+        <p className="self-center text-sm">
+          {imageError ? (<span className="text-red-700">Error uploading image</span>) :
+            imagePercent > 0 && imagePercent < 100 ? (<span className="text-slate-700">{`Uploading image...${imagePercent}%`}</span>) :
+              imagePercent === 100 ? (<span className="text-green-600">Image Uploaded Successfully</span>) : ("")}
+         </p>
+        <input defaultValue={currentUser.name} type="text" placeholder="Username" id="name" className="bg-slate-100 p-3 rounded-lg" />
          <input defaultValue={currentUser.email} type="email" placeholder="Email" id="email" className="bg-slate-100 p-3 rounded-lg" />
          <input type="password" placeholder="Password" id="password" className="bg-slate-100 p-3 rounded-lg"/>
          <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-90 disabled:opacity-70">Update</button>
@@ -18,4 +57,22 @@ export default function Profile() {
       </div>
     </div>
   )
+}
+
+
+{
+  /*
+  Firebase storage rules configuration for uploading image
+  service firebase.storage {
+  match /b/{bucket}/o {
+    match /{allPaths=**} {
+      allow read;
+      allow write: if
+      request.resource.size < 2 *1024 * 1024 &&
+      request.resource.contentType.matches('image/.*')
+      
+    }
+  }
+}
+  */
 }
