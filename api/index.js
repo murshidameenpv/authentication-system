acbe2c4a2f4a196fb870c6b17422165c5d034a84
murshidameen <murshidmonzpv@gmail.com>
2023-11-03 16:03:40 +0530

    add pre-production changes

--- api/index.js
+++ api/index.js
@@ -4,14 +4,20 @@ import './db/mongodb.js';
 import cookieParser from 'cookie-parser';
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
+import path from 'path';
     
 dotenv.config()
 const app = express()
 const port = process.env.PORT || 8001;
+const __dirname = path.resolve()
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname,'/client/dist')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client','dist','index.html'))
+})
 
 app.use('/api/user/', userRouter)
 app.use('/api/auth/', authRouter)
