ef7ed0416167c1b142100c0d83b9fed427e69eff
murshidameen <murshidmonzpv@gmail.com>
2023-11-03 15:52:19 +0530

    completed home,about pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,19 @@ import React from "react";
 
 export default function About() {
     return (
-        <div>About</div>
+          <div className="px-4 py-12 max-w-2xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-600">
+       About
+      </h1>
+      <p className="mb-4 text-slate-400">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <p className="mb-4 text-slate-400">
+      Ultricies lacus sed turpis tincidunt id aliquet risus feugiat. Placerat in egestas erat imperdiet sed euismod nisi porta lorem. Velit sed ullamcorper morbi tincidunt ornare massa. Placerat duis ultricies lacus sed turpis tincidunt. Mauris a diam maecenas sed enim. Facilisi cras fermentum odio eu feugiat pretium nibh ipsum consequat. Ultricies leo integer malesuada nunc vel risus. Ut porttitor leo a diam sollicitudin tempor id eu nisl.
+      </p>
+      <p className="mb-4 text-slate-400">
+       Interdum consectetur libero id faucibus nisl tincidunt eget nullam. Aliquet lectus proin nibh nisl condimentum id. A condimentum vitae sapien pellentesque habitant morbi tristique. Cras ornare arcu dui vivamus arcu felis bibendum. Sit amet consectetur adipiscing elit duis tristique sollicitudin nibh sit. In est ante in nibh. Eu scelerisque felis imperdiet proin fermentum leo vel orci. Pellentesque habitant morbi tristique senectus et netus et malesuada fames. Gravida cum sociis natoque penatibus et magnis dis.
+      </p>
+    </div>
     )
 }
